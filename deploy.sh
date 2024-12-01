8ac763ac81096c84c51f15242a5c82402cd9130c
Chidi <mylandmarktech@gmail.com>
2024-12-01 13:12:33 -0600

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 developers at work
 application is ready to be deployed to the development envt. 
 deployment must be reviewed by managers before deployment.
-
+SDLC is great working application experiance  
+AWS is a great deployment server tool.
