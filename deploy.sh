323bd7aaf62b394c123f238053296d23a5ef23a6
Chidi <mylandmarktech@gmail.com>
2024-12-01 13:14:26 -0600

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 developers at work
 application is ready to be deployed to the development envt. 
 deployment must be reviewed by managers before deployment.
-
+AWS is a great cloud deployment server.
